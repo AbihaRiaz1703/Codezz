69a7e4ffdefeb60e8152de786240bada7a4f0354
AbihaRiaz1703 <ramshariaz039@gmail.com>
2024-11-19 19:15:56 +0500

    test comment

--- lib/n.dart
+++ lib/n.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:my_app/o.dart';
 import 'package:my_app/p.dart';
 
@@ -13,69 +14,71 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: const Text(
-            'here i am',
-            style: TextStyle(color: Colors.white),
-          ),
-        ),
-        backgroundColor: const Color.fromARGB(255, 6, 43, 80),
-      ),
-      drawer: Drawer(
-        backgroundColor: Colors.white,
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            UserAccountsDrawerHeader(
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 6, 43, 80),
-                ),
-                currentAccountPicture: CircleAvatar(
-                  backgroundImage: AssetImage('images/suii.avif'),
-                ),
-                accountName: Text('AbihaRiaz'),
-                accountEmail: Text('iamhere@gmail.com')),
-            ListTile(
-              leading: Icon(Icons.import_contacts),
-              onTap: () {
-                Navigator.pushNamed(context, No.id);
-              },
-              title: Text('About Me'),
-              subtitle: Text('Click here'),
+    return DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Center(
+              child: const Text(
+                'here i am',
+                style: TextStyle(color: Colors.white),
+              ),
             ),
-            ListTile(
-              leading: Icon(Icons.logout),
-              onTap: () {
-                Navigator.pushNamed(context, Hello.id);
-              },
-              title: Text('Lod Out?'),
-              subtitle: Text('Click here'),
-            )
-          ],
-        ),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Center(
-            child: TextButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, Hello.id);
-                  //!can be done with the code given below but that
-                  //!code is a bit long and difficult
-                  // Navigator.push(
-                  //     context,
-                  //     MaterialPageRoute(
-                  //       builder: (context) => Hello(),
-                  //     ));
-                },
-                child: const Text('move to next')),
-          )
-        ],
-      ),
-    );
+            backgroundColor: const Color.fromARGB(255, 6, 43, 80),
+          ),
+          drawer: Drawer(
+            backgroundColor: Colors.white,
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                UserAccountsDrawerHeader(
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 6, 43, 80),
+                    ),
+                    currentAccountPicture: CircleAvatar(
+                      backgroundImage: AssetImage('images/suii.avif'),
+                    ),
+                    accountName: Text('AbihaRiaz'),
+                    accountEmail: Text('iamhere@gmail.com')),
+                ListTile(
+                  leading: Icon(Icons.auto_awesome),
+                  onTap: () {
+                    Navigator.pushNamed(context, No.id);
+                  },
+                  title: Text('About Me'),
+                  subtitle: Text('Click here'),
+                ),
+                ListTile(
+                  leading: Icon(FontAwesomeIcons.outdent),
+                  onTap: () {
+                    Navigator.pushNamed(context, Hello.id);
+                  },
+                  title: Text('Log Out?'),
+                  subtitle: Text('Click here'),
+                )
+              ],
+            ),
+          ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, Hello.id);
+                      //!can be done with the code given below but that
+                      //!code is a bit long and difficult
+                      // Navigator.push(
+                      //     context,
+                      //     MaterialPageRoute(
+                      //       builder: (context) => Hello(),
+                      //     ));
+                    },
+                    child: const Text('move to next')),
+              )
+            ],
+          ),
+        ));
   }
 }
--- lib/o.dart
+++ lib/o.dart
@@ -21,18 +21,15 @@ class _HelloState extends State<Hello> {
         ),
         backgroundColor: Color.fromARGB(255, 6, 43, 80),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Center(
-            child: TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Text('now if you blink you\'re a maths lover')),
-          )
-        ],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text(
+                  "A good introduction should identify your topic, provide essential context, and indicate your particular focus in the essay. It also needs to engage your readers' interest. A strong conclusion will provide a sense of closure to the essay while again placing your concepts in a somewhat wider context.The introduction typically describes the scope of the document and gives a brief explanation or a summary of the document. It may also explain certain elements that are important to the document. The readers can thus have an idea about the following text before they actually start reading it.The introduction typically describes the scope of the document and gives a brief explanation or a summary of the document. It may also explain certain elements that are important to the document. The readers can thus have an idea about the following text before they actually start reading it.The introduction typically describes the scope of the document and gives a brief explanation or a summary of the document. It may also explain certain elements that are important to the document. The readers can thus have an idea about the following text before they actually start reading it.The introduction typically describes the scope of the document and gives a brief explanation or a summary of the document. It may also explain certain elements that are important to the document. The readers can thus have an idea about the following text before they actually start reading it.The introduction typically describes the scope of the document and gives a brief explanation or a summary of the document. It may also explain certain elements that are important to the document. The readers can thus have an idea about the following text before they actually start reading it.The introduction typically describes the scope of the document and gives a brief explanation or a summary of the document. It may also explain certain elements that are important to the document. The readers can thus have an idea about the following text before they actually start reading it."),
+            ],
+          ),
+        ),
       ),
     );
   }
