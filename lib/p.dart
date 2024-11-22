14138d652d8bd2a371cfd477b5e78c6847c01988
AbihaRiaz1703 <ramshariaz039@gmail.com>
2024-11-22 16:52:30 +0500

    test comment

--- lib/p.dart
+++ lib/p.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 
 class No extends StatefulWidget {
@@ -12,6 +13,7 @@ class _NoState extends State<No> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Color.fromARGB(255, 41, 125, 209),
         appBar: AppBar(
           title: Text(
             'Bella ciao',
@@ -23,9 +25,32 @@ class _NoState extends State<No> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(
-              'hello world',
-              style: TextStyle(color: Colors.pink),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                const SizedBox(width: 20.0, height: 100.0),
+                const Text(
+                  'Be',
+                  style: TextStyle(fontSize: 43.0),
+                ),
+                const SizedBox(width: 20.0, height: 100.0),
+                DefaultTextStyle(
+                  style: const TextStyle(
+                    fontSize: 40.0,
+                    fontFamily: 'Horizon',
+                  ),
+                  child: AnimatedTextKit(
+                    animatedTexts: [
+                      RotateAnimatedText('AWESOME'),
+                      RotateAnimatedText('OPTIMISTIC'),
+                      RotateAnimatedText('DIFFERENT'),
+                    ],
+                    onTap: () {
+                      print("Tap Event");
+                    },
+                  ),
+                ),
+              ],
             )
           ],
         ));
